29b8fcd9e96b4bab94ce81bb59fc671e9aaa8ebc
Emerald <87787233+0AceSpades0@users.noreply.github.com>
2025-06-22 09:28:38 -0700

    Update PauseSubState.hx

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -169,6 +169,7 @@ class PauseSubState extends MusicBeatSubstate
 		blueballedTxt.alpha = 0;
 		//levelDifficulty.alpha = 0;
 		levelInfo.alpha = 0;
+		sprDifficulty.alpha = 0;
 
 		levelInfo.x = FlxG.width - (levelInfo.width + 20);
 		//levelDifficulty.x = FlxG.width - (levelDifficulty.width + 20);
@@ -178,6 +179,7 @@ class PauseSubState extends MusicBeatSubstate
 		FlxTween.tween(levelInfo, {alpha: 1, y: 20}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.3});
 		//FlxTween.tween(levelDifficulty, {alpha: 1, y: levelDifficulty.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.5});
 		FlxTween.tween(blueballedTxt, {alpha: 1, y: blueballedTxt.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.7});
+		FlxTween.tween(sprDifficulty, {alpha: 1}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.7});
 
 		grpMenuShit = new FlxTypedGroup<Alphabet>();
 		add(grpMenuShit);
